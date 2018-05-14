0e9683002658ff6f87720dec286174623e1b39bd
Sean Oughton <seanoughton@gmail.com>
2018-05-14 22:44:26 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    byebug
+    user = User.new(user_params).save
   end
 
   def show
-    
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+
   end
 
 end
